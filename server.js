87249c00d76c864f275be77a3b18a7206c2c5ed4
Miguel Alicea <malicea0783@gmail.com>
2021-06-19 14:12:11 -0500

    updated delete fucntion

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
+// Dependencies
 const express = require("express");
-const uniqid = require('uniqid');
 const path = require("path");
-const fs = require("fs")
-const db = require("./db.json")
-
+const fs = require("fs");
+const uniqid = require('uniqid');
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3001;
 //server settings (middleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(express.static("public"))
-
+//html routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-
+//api routes
 app.get('/api/notes', (req, res) => {
     fs.readFile("./db.json", (err, data) => {
-        if(err) throw err;
+        if (err) throw err;
         res.json(JSON.parse(data))
     })
-
 })
-
 app.post('/api/notes', (req, res) => {
-    let data = req.body;
-    data["id"] = uniqid();
-    db.push(req.body)
-
-    fs.writeFile("./db.json", JSON.stringify(db), err => {
+    let newNote = req.body;
+    newNote.id = uniqid();
+    fs.readFile("./db.json", "utf8", (err, data) => {
         if (err) throw err;
-        res.json(data)
+        const parseData = JSON.parse(data);
+        console.log(JSON.parse(data));
+        parseData.push(newNote);
+        fs.writeFile("./db.json", JSON.stringify(parseData), err => {
+            if (err) throw err;
+            res.json(parseData)
+        })
     })
 })
-
+//get the notes, filter the notes via id, and then delete it
 app.delete('/api/notes/:id', (req, res) => {
     fs.readFile("./db.json", (err, data) => {
         if (err) throw err;
@@ -44,5 +44,4 @@ app.delete('/api/notes/:id', (req, res) => {
         });
     })
 })
-
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
